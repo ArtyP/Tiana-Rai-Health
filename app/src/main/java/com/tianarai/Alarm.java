2e8b4f59d3c573d02883bf9e443dedee230ae660
ArtyP <artur.pasich@outlook.com>
2020-08-15 12:53:00 +0200

    Code clean up

--- app/src/main/java/com/tianarai/Alarm.java
+++ app/src/main/java/com/tianarai/Alarm.java
@@ -25,51 +25,10 @@ public class Alarm extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        /*PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "myapp:mywakelocktag");
-        wl.acquire();
-
-        // Put here YOUR code.
-        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        nm.notify("Notificatiob", );*/
-
         Toast.makeText(context, "Alarm:onReceive 1", Toast.LENGTH_SHORT).show();
-        /*Snackbar.make(context, "MainActivity", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();*/
-
-        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Name";
-            String description = "Description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("222", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }*/
-
-        /*NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        // notificationId is a unique int for each notification that you must define
-        notificationManager.notify(111, builder.build());*/
-
-        //wl.release();
-        //CreateFullScreenNotification(context);
 
         //if (FullscreenActivity.FULL_SCREEN_ACTION.equals(intent.getAction()))
             FullscreenActivity.CreateFullScreenNotification(context);
-        //context.startActivity(intent);
-        /*Intent alarmIntent = new Intent(context, FullscreenActivity.class);
-        context.startActivity(alarmIntent);
-        context.finish();*/
-
-        /*Intent i = new Intent(context,FullscreenActivity.class);
-        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(i);*/
-
-        //start activity
-        /*intent.setComponent(new ComponentName("com.tianarai", "com.tianarai.FullscreenActivity"));
-        context.startActivity(intent);*/
 
         Toast.makeText(context, "Alarm:onReceive 2", Toast.LENGTH_SHORT).show();
     }
@@ -78,16 +37,7 @@ public class Alarm extends BroadcastReceiver
     public void setAlarm(Context context)
     {
 
-        /*AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        Intent i = new Intent(context, Alarm.class);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+1000, 1000, pi); // Millisec * Second * Minute
-
-         */
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-
-        /*Intent alarmIntent = new Intent(context, Alarm.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);*/
 
         Intent alarmIntent = new Intent(context, Alarm.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, alarmIntent, 0);
@@ -149,35 +99,6 @@ public class Alarm extends BroadcastReceiver
             notificationManager.createNotificationChannel(channel);
         }
     }
-
-    static void CreateFullScreenNotification(Context context) {
-        Intent intent = new Intent(context, Alarm.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_USER_ACTION | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder notificationBuilder =
-                new NotificationCompat.Builder(context, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle("Full Screen Alarm Test")
-                        .setContentText("This is a test")
-                        .setPriority(NotificationCompat.PRIORITY_MAX)
-                        .setCategory(NotificationCompat.CATEGORY_ALARM)
-                        .setContentIntent(pendingIntent)
-                        .setFullScreenIntent(pendingIntent, true);
-        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notificationBuilder.build());
-    }
-
-    /*private static void createNotificationChannel(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-
-            if (notificationManager.getNotificationChannel(CHANNEL_ID) == null) {
-                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "channel_name", NotificationManager.IMPORTANCE_HIGH);
-                channel.setDescription("channel_description");
-                notificationManager.createNotificationChannel(channel);
-            }
-        }
-    }*/
 
     public void cancelAlarm(Context context)
     {
